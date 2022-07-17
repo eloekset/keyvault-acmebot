e47e65b1aad670cddaa9811ed5d5619d089d22b5
Eivind Gussiås Løkseth <eloekset@msn.com>
2022-07-17 13:00:09 +0200

    Use file-scoped namespace

--- KeyVault.Acmebot.Tests/Providers/DomeneShopProvider_ListZonesAsyncShould.cs
+++ KeyVault.Acmebot.Tests/Providers/DomeneShopProvider_ListZonesAsyncShould.cs
@@ -16,58 +16,56 @@ using Shouldly;
 
 using Xunit;
 
-namespace KeyVault.Acmebot.Tests.Providers
+namespace KeyVault.Acmebot.Tests.Providers;
+public class DomeneShopProvider_ListZonesAsyncShould
 {
-    public class DomeneShopProvider_ListZonesAsyncShould
+    private DomeneShopProvider _provider;
+    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+
+    public DomeneShopProvider_ListZonesAsyncShould()
     {
-        private DomeneShopProvider _provider;
-        private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
-
-        public DomeneShopProvider_ListZonesAsyncShould()
+        var options = new DomeneShopOptions
         {
-            var options = new DomeneShopOptions
-            {
-                ApiKeyUser = "test",
-                ApiKeyPassword = "test",
-                PropagationSeconds = 60
-            };
-            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
-            var httpClient = new HttpClient(_mockHttpMessageHandler.Object);
-            _provider = new DomeneShopProvider(options, httpClient);
-        }
+            ApiKeyUser = "test",
+            ApiKeyPassword = "test",
+            PropagationSeconds = 60
+        };
+        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+        _provider = new DomeneShopProvider(options, httpClient);
+    }
 
-        [Fact]
-        public async Task ReturnListOfDomains()
+    [Fact]
+    public async Task ReturnListOfDomains()
+    {
+        // Arrange
+        var responseData = TestData.ReadResourceAsString(TestData.DomeneShopDomainsResponse_sample1);
+        _mockHttpMessageHandler.Protected().As<IHttpMessageHandler>()
+            .Setup(x => x.SendAsync(
+                It.Is<HttpRequestMessage>(r =>
+                r.Method == HttpMethod.Get &&
+                r.RequestUri == new Uri($"{DomeneShopConstants.BaseUri}domains")),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(CreateHttpResponseMessage(HttpStatusCode.OK, responseData));
+
+        // Act
+        var actualDnsZones = await _provider.ListZonesAsync();
+
+        // Assert
+        actualDnsZones.ShouldNotBeNull("An object was returned");
+        actualDnsZones.Count.ShouldBe(3, "Three domains were returned");
+        foreach (var zone in actualDnsZones)
         {
-            // Arrange
-            var responseData = TestData.ReadResourceAsString(TestData.DomeneShopDomainsResponse_sample1);
-            _mockHttpMessageHandler.Protected().As<IHttpMessageHandler>()
-                .Setup(x => x.SendAsync(
-                    It.Is<HttpRequestMessage>(r =>
-                    r.Method == HttpMethod.Get &&
-                    r.RequestUri == new Uri($"{DomeneShopConstants.BaseUri}domains")),
-                    It.IsAny<CancellationToken>()))
-                .ReturnsAsync(CreateHttpResponseMessage(HttpStatusCode.OK, responseData));
-
-            // Act
-            var actualDnsZones = await _provider.ListZonesAsync();
-
-            // Assert
-            actualDnsZones.ShouldNotBeNull("An object was returned");
-            actualDnsZones.Count.ShouldBe(3, "Three domains were returned");
-            foreach (var zone in actualDnsZones)
-            {
-                zone.Id.ShouldNotBeNullOrWhiteSpace("Id has been populated");
-                zone.Name.ShouldNotBeNullOrWhiteSpace("Name has been populated");
-            }
+            zone.Id.ShouldNotBeNullOrWhiteSpace("Id has been populated");
+            zone.Name.ShouldNotBeNullOrWhiteSpace("Name has been populated");
         }
+    }
 
-        private HttpResponseMessage CreateHttpResponseMessage(HttpStatusCode httpStatusCode, string responseString)
-        {
-            var response = new HttpResponseMessage(httpStatusCode);
-            response.Content = new StringContent(responseString, Encoding.UTF8, "application/json");
+    private HttpResponseMessage CreateHttpResponseMessage(HttpStatusCode httpStatusCode, string responseString)
+    {
+        var response = new HttpResponseMessage(httpStatusCode);
+        response.Content = new StringContent(responseString, Encoding.UTF8, "application/json");
 
-            return response;
-        }
+        return response;
     }
 }
