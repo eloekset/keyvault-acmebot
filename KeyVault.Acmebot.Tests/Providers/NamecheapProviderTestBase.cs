45adea8c4c9a299190c0c52945bca06af3f25f6b
Eivind Gussiås Løkseth <eivind@outlook.com>
2022-07-18 20:29:15 +0200

    Add support for namecheap (#6)
    * Add support for Namecheap
    
    * Add tests for Namecheap ListZones
    
    * Add tests for Namecheap CreateTxtRecord
    
    * Merge changes to NamecheapProvider
    
    * Fix bug in address to ipfy.org and parsing of warnings
    
    * Add tests for Namecheap DeleteTxtRecord
    
    * Cleanup tests

---
+++ KeyVault.Acmebot.Tests/Providers/NamecheapProviderTestBase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+using KeyVault.Acmebot.Options;
+using KeyVault.Acmebot.Providers;
+
+using Moq;
+using Moq.Contrib.HttpClient;
+using Moq.Protected;
+
+namespace KeyVault.Acmebot.Tests.Providers;
+public class NamecheapProviderTestBase
+{
+    protected readonly NamecheapOptions _options;
+    protected readonly NamecheapProvider _provider;
+    protected readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+    protected readonly Mock<HttpMessageHandler> _mockIpfyOrgHttpMessageHandler;
+
+    public NamecheapProviderTestBase()
+    {
+        _options = new NamecheapOptions
+        {
+            ApiUser = NamecheapConstants.ApiUser1,
+            ApiKey = NamecheapConstants.ApiKey1
+        };
+        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        var httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+        _mockIpfyOrgHttpMessageHandler = new Mock<HttpMessageHandler>();
+        MockNormalIpfyOrgResponse();
+        var ipfyOrgHttpClient = new HttpClient(_mockIpfyOrgHttpMessageHandler.Object);
+        _provider = new NamecheapProvider(_options, httpClient, ipfyOrgHttpClient);
+    }
+
+    protected Uri CreateRequestUri(string apiUser, string apiKey, string clientIp, string command, int? page = null)
+    {
+        string queryString = NamecheapConstants.AllCommandsTemplate
+            .Replace(NamecheapConstants.ApiUserTemplate, apiUser)
+            .Replace(NamecheapConstants.ApiKeyTemplate, apiKey)
+            .Replace(NamecheapConstants.ClientIdTemplate, clientIp)
+            .Replace(NamecheapConstants.CommandTemplate, command);
+
+        if (page != null)
+        {
+            queryString += $"&Page={page}";
+        }
+
+        return new Uri(new Uri(NamecheapConstants.BaseUri), queryString);
+    }
+
+    protected void MockNormalIpfyOrgResponse(string ipAddress = NamecheapConstants.ClientIp1)
+    {
+        _mockIpfyOrgHttpMessageHandler.Protected().As<IHttpMessageHandler>()
+            .Setup(x => x.SendAsync(
+                It.IsAny<HttpRequestMessage>(),
+                It.IsAny<CancellationToken>()
+                )
+            ).ReturnsAsync(CreateHttpResponseMessageJsonString(CreateIpfyOrgJsonResponse(ipAddress)));
+    }
+
+    protected string CreateIpfyOrgJsonResponse(string ipAddress)
+    {
+        return
+@"{
+    ""ip"": """ + ipAddress + @"""
+}";
+    }
+
+    protected HttpResponseMessage CreateHttpResponseMessageJsonString(string responseString, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+    {
+        var response = new HttpResponseMessage(httpStatusCode);
+        response.Content = new StringContent(responseString, Encoding.UTF8, "application/json");
+
+        return response;
+    }
+
+    protected HttpResponseMessage CreateHttpResponseMessageXmlString(string responseString, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+    {
+        var response = new HttpResponseMessage(httpStatusCode);
+        response.Content = new StringContent(responseString, Encoding.UTF8, "application/xml");
+
+        return response;
+    }
+}
--- KeyVault.Acmebot.Tests/TestData.cs
+++ KeyVault.Acmebot.Tests/TestData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Xml;
 
 namespace KeyVault.Acmebot.Tests;
 
@@ -6,10 +7,37 @@ internal class TestData
 {
     public const string DomeneShopDomainsResponse_sample1 = "DomeneShopDomainsResponse_sample1";
     public const string DomeneShopDnsRecordsResponse_example1_com_sample1 = "DomeneShopDnsRecordsResponse_example1_com_sample1";
+    public const string Namecheap_InvalidRequestIP_sample1 = "Namecheap_InvalidRequestIP_sample1";
+    public const string NamecheapDomainsResponse_singlepage = "NamecheapDomainsResponse_singlepage";
+    public const string NamecheapDomainsResponse_page1of2 = "NamecheapDomainsResponse_page1of2";
+    public const string NamecheapDomainsResponse_page2of2 = "NamecheapDomainsResponse_page2of2";
+    public const string NamecheapDomainsResponse_page3of2 = "NamecheapDomainsResponse_page3of2";
+    public const string NamecheapDomainsResponse_page1of3 = "NamecheapDomainsResponse_page1of3";
+    public const string NamecheapDomainsResponse_page2of3 = "NamecheapDomainsResponse_page2of3";
+    public const string NamecheapDomainsResponse_page3of3 = "NamecheapDomainsResponse_page3of3";
+    public const string NamecheapDomainsResponse_page4of3 = "NamecheapDomainsResponse_page4of3";
+    public const string NamecheapGetHostsResponse_example1_com_sample1 = "NamecheapGetHostsResponse_example1_com_sample1";
+    public const string NamecheapSetHostsResponse_example1_success = "NamecheapSetHostsResponse_example1_success";
+    public const string NamecheapSetHostsResponse_example1_failure = "NamecheapSetHostsResponse_example1_failure";
 
     public static string ReadResourceAsString(string resourceKey)
     {
         byte[] data = Properties.Resources.ResourceManager.GetObject(resourceKey) as byte[];
+
+        if (data == null)
+        {
+            return Properties.Resources.ResourceManager.GetString(resourceKey);
+        }
+
         return Encoding.UTF8.GetString(data);
+    }
+
+    public static XmlDocument GetResourceAsXmlDocument(string resourceKey)
+    {
+        XmlDocument xmlDocument = new XmlDocument() { PreserveWhitespace = true };
+        string xml = ReadResourceAsString(resourceKey);
+        xmlDocument.LoadXml(xml);
+
+        return xmlDocument;
     }
 }
